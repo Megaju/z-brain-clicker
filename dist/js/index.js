2fcd3d83853f26d649bcd89f632fba887f9b2fc7
Megaju <julien@malle.pro>
2018-11-25 14:41:25 +0100

    buy zombie + zombie add brains per sec

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,6 +1,17 @@
 var brainEl = document.getElementById('big-big-brain');
 var nbBrainEl = document.getElementById('nbBrain');
+var zMarket = document.getElementById('z-market');
+var zombieCase = document.getElementById('zombie');
+zombieCase.addEventListener('click', function () {
+    buyZombie();
+});
 var brain = 0;
+var zombie = {
+    nbZombie: 0,
+    brainPerSec: 0,
+    price: 25
+};
+displayZombieCase();
 displayNbBrain();
 brainEl.addEventListener('click', function () {
     brain++;
@@ -8,7 +19,7 @@ brainEl.addEventListener('click', function () {
     brainAnimation();
 });
 function displayNbBrain() {
-    nbBrainEl.innerHTML = "" + brain;
+    nbBrainEl.innerHTML = "" + Math.round(brain);
 }
 function brainAnimation() {
     brainEl.style.transform = 'scale(1.2)';
@@ -16,3 +27,39 @@ function brainAnimation() {
         brainEl.style.transform = 'scale(1)';
     }, 100);
 }
+function start() {
+    var timeline = setInterval(function () {
+        brain += zombie.brainPerSec;
+        displayNbBrain();
+    }, 1000);
+}
+function buyZombie() {
+    if (brain >= zombie.price) {
+        brain -= zombie.price;
+        displayNbBrain();
+        zombie.nbZombie++;
+        zombie.brainPerSec += 0.1;
+        zombie.brainPerSec = Math.round(zombie.brainPerSec * 10) / 10;
+        zombie.price = Math.round(zombie.price + (zombie.nbZombie * 1.75));
+        displayZombieCase();
+    }
+}
+function displayZombieCase() {
+    var priceEl = document.querySelector('#zombie #price');
+    var brainPerSec = document.getElementById('brainPerSec');
+    priceEl.innerHTML = "" + zombie.price;
+    brainPerSec.innerHTML = zombie.brainPerSec + " Brains per second";
+}
+start();
+/**
+ *
+ * Il va falloir enregistrer le nombre de zombie
+ * Ainsi que leur prix croissant
+ * Ceci doit aussi modifier la variable qui augmente le nb de brain par seconde
+ *
+ * Il faudrait enregistrer tout ça dans un objet avec :
+ * nbZombie: number
+ * brainPerSec: number
+ * price: number
+ *
+ */ 
--- ts/index.ts
+++ ts/index.ts
@@ -1,7 +1,21 @@
 const brainEl = document.getElementById('big-big-brain');
 const nbBrainEl = document.getElementById('nbBrain');
 
+const zMarket = document.getElementById('z-market');
+const zombieCase = document.getElementById('zombie');
+
+zombieCase.addEventListener('click', function() {
+  buyZombie();
+});
+
 let brain = 0;
+
+const zombie: zombie = {
+  nbZombie: 0,
+  brainPerSec: 0,
+  price: 25
+}
+displayZombieCase();
 
 displayNbBrain();
 
@@ -12,7 +26,7 @@ brainEl.addEventListener('click', function() {
 });
 
 function displayNbBrain() {
-  nbBrainEl.innerHTML = `${brain}`;
+  nbBrainEl.innerHTML = `${Math.round(brain)}`;
 }
 
 function brainAnimation() {
@@ -21,3 +35,50 @@ function brainAnimation() {
     brainEl.style.transform = 'scale(1)';
   }, 100);
 }
+
+function start() {
+  const timeline = setInterval(() => {
+    brain += zombie.brainPerSec;
+    displayNbBrain();
+  }, 1000);
+}
+
+function buyZombie() {
+  if (brain >= zombie.price) {
+    brain -= zombie.price;
+    displayNbBrain();
+    zombie.nbZombie++;
+    zombie.brainPerSec += 0.1;
+    zombie.brainPerSec = Math.round(zombie.brainPerSec * 10) / 10;
+    zombie.price = Math.round(zombie.price + (zombie.nbZombie * 1.75));
+    displayZombieCase();
+  }
+}
+
+function displayZombieCase() {
+  const priceEl = document.querySelector('#zombie #price');
+  const brainPerSec = document.getElementById('brainPerSec');
+  priceEl.innerHTML = `${zombie.price}`;
+  brainPerSec.innerHTML = `${zombie.brainPerSec} Brains per second`;
+}
+
+interface zombie {
+  nbZombie: number;
+  brainPerSec: number;
+  price: number;
+}
+
+start();
+
+/**
+ * 
+ * Il va falloir enregistrer le nombre de zombie
+ * Ainsi que leur prix croissant
+ * Ceci doit aussi modifier la variable qui augmente le nb de brain par seconde
+ * 
+ * Il faudrait enregistrer tout ça dans un objet avec :
+ * nbZombie: number
+ * brainPerSec: number
+ * price: number
+ * 
+ */
