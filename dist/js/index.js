205e3a1f2ce740f6956f343f331d839b0f7f6384
Megaju <julien@malle.pro>
2018-11-25 13:51:36 +0100

    brain click -> better animation

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,10 +1,18 @@
 var brainEl = document.getElementById('big-big-brain');
 var nbBrainEl = document.getElementById('nbBrain');
 var brain = 0;
+displayNbBrain();
 brainEl.addEventListener('click', function () {
     brain++;
     displayNbBrain();
+    brainAnimation();
 });
 function displayNbBrain() {
     nbBrainEl.innerHTML = "" + brain;
+}
+function brainAnimation() {
+    brainEl.style.transform = 'scale(1.2)';
+    setTimeout(function () {
+        brainEl.style.transform = 'scale(1)';
+    }, 100);
 }
--- ts/index.ts
+++ ts/index.ts
@@ -3,11 +3,21 @@ const nbBrainEl = document.getElementById('nbBrain');
 
 let brain = 0;
 
+displayNbBrain();
+
 brainEl.addEventListener('click', function() {
   brain++;
   displayNbBrain();
+  brainAnimation();
 });
 
 function displayNbBrain() {
   nbBrainEl.innerHTML = `${brain}`;
+}
+
+function brainAnimation() {
+  brainEl.style.transform = 'scale(1.2)';
+  setTimeout(() => {
+    brainEl.style.transform = 'scale(1)';
+  }, 100);
 }
